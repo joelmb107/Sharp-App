4f9eac30e771d7c0997a32e032b949dfb3ba1559
Your Name <you@example.com>
2026-05-25 10:09:44 +0100

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options("/", cors(corsOptions));
 app.use(express.json());
 
 const openai = new OpenAI({
